1e4b4be806318392290f2d51e467c6823b657877
Mario Loyola <mar.loyolac@duocuc.cl>
2026-05-03 00:28:51 -0400

    Correcion
    Se corrigio un error que no permitia ver las paginas creadas sin iniciar sesion

--- backend/src/main/java/com/backend/pymeweb/security/SecurityConfig.java
+++ backend/src/main/java/com/backend/pymeweb/security/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.backend.pymeweb.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -31,6 +32,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/secciones/negocio/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
